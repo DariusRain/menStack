a11a8601dffd3fbe811fe0538ebffa782f61eb7b
johnfruscella <johnfruscella@gmail.com>
2020-03-05 19:32:27 -0500

    Added hotels.js file to routes and added code to index.js to connect server

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const PORT = process.env.PORT || 3000;
+const hotels = require("./routes/api/hotels")
 const keys = require("./config/keys");
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 mongoose
   .connect(keys.mongoUri,  { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("connected to db"))
   .catch(error => console.log("DB Connection error", error));
 
-app.use(express.static("public"));
+  app.use('/api/hotels/', hotels);
 
-app.listen(3000, () => console.log("listening on port 3000"));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -6,32 +6,34 @@ const isEmpty = require("is-empty");
 //Get All Hotels
 router.get("/getallhotels", async (req, res) => {
     const hotels = await hotel.find();
-    if(!hotels) return res.status(404).send(`No hotels found`);
+    if (!hotels) return res.status(404).send(`No hotels found`);
     res.send(hotels);
 });
 
 //Get One Hotel
-router.get("/:userName", async (req, res) => {
+router.get("/:hotelName", async (req, res) => {
     const hotelName = req.params.hotelName;
-    const errors = {};
+    //const errors = {};
     const aHotel = await Hotel.find({ hotelName: hotelName });
-    if(isEmpty(aHotel)){
-       return res.status(404).json("This hotel could not be found.");
+    if (isEmpty(aHotel)) {
+        return res.status(404).json("This hotel could not be found.");
     };
     res.send(aHotel);
 });
 //Create Hotel  POST
 router.post("/", async (req, res) => {
-    const { hotelName, location, phoneNumber } = req.body;
+    const { hotelName, city, phoneNumber } = req.body;
 
-    if(!hotelName || !location || !phoneNumber) {
+    if (!hotelName || !city || !state || !street || !phoneNumber) {
         return res.status(400).send("Input field is missing");
     }
 
     let newHotel = new Hotel({
         hotelName,
-        location,
-        phoneNumber 
+        city,
+        state,
+        street,
+        phoneNumber
     });
 
     newHotel = await newHotel.save();
@@ -40,13 +42,15 @@ router.post("/", async (req, res) => {
 
 //Update Hotel 
 router.put('/:id', async (req, res) => {
-    const updatedhotel = await hotel.findByIdAndUpdate(req.params.id,{
-        hotelName: req.body.userName,
-        location: req.body.email,
-        phoneNumber: req.body.hotel
+    const updatedhotel = await hotel.findByIdAndUpdate(req.params.id, {
+        hotelName: req.body.hotelName,
+        city: req.body.city,
+        state: req.body.state,
+        street: req.body.street,
+        phoneNumber: req.body.phoneNumber
     }, { new: true });
 
-    if(!updatedhotel) return res.status(404).send(`No hotel found with that ID`);
+    if (!updatedhotel) return res.status(404).send(`No hotel found with that ID`);
 
     res.json(updatedhotel);
 
@@ -56,7 +60,7 @@ router.put('/:id', async (req, res) => {
 // DELETE Hotel
 router.delete("/:id", async (req, res) => {
     const hotel = await hotel.findByIdAndRemove(req.params.id);
-    if(!hotel) return res.status(404).send(`No hotel found`);
+    if (!hotel) return res.status(404).send(`No hotel found`);
     res.json(hotel);
 })
 
